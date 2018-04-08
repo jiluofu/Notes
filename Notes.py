f9873a36b49a17717694a8dec7034455750f985a
zhuxu <zhuxu@baidu.com>
2018-04-08 11:54:31 +0800

    =sublime标注kindle工具

---
+++ Notes.py
@@ -0,0 +1,90 @@
+import sublime
+import sublime_plugin
+import re
+
+
+class ExampleCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Hello, World!")
+
+class NotesFormatCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+
+        self.view.run_command('select_all')
+        content = self.view.substr(self.view.sel()[0]).strip()
+
+        # 清理duokan日期标签
+        pa = r'^[\d]*-[\d]*-[\d]* [\d]*:[\d]*:[\d]*[\r\n]*$'
+        pa = re.compile(pa, re.M)
+        content = re.sub(pa, '', content)
+
+        # 清理kindle日期标签
+        pa = r'^(标注|Highlight)\((.)*\)(.)*$'
+        pa = re.compile(pa, re.M)
+        content = re.sub(pa, '', content)
+
+        pa = r'^(书签|Bookmark) - (.)*$'
+        pa = re.compile(pa, re.M)
+        content = re.sub(pa, '', content)
+
+
+        content = content.replace('　', '')
+        content = content.replace(' ', '')
+
+        print(content)
+
+        self.view.run_command('cut')
+        self.view.insert(edit, 0, content)
+        sublime.set_clipboard('')
+
+
+class NotesAddQuoteCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+
+        self.view.run_command('select_all')
+        content = self.view.substr(self.view.sel()[0]).strip()
+
+        pa = r'[\n]'
+        pa = re.compile(pa, re.M)
+        content = re.sub(pa, '\n>', content)
+        content = '>' + content
+
+        pa = r'\n>\n>\n>'
+        pa = re.compile(pa, re.M)
+        content = re.sub(pa, '\n\n\n>', content)
+
+        pa = r'>【】'
+        pa = re.compile(pa, re.M)
+        content = re.sub(pa, '\n', content)
+
+
+        print(content)
+
+        self.view.run_command('cut')
+        self.view.insert(edit, 0, content)
+        sublime.set_clipboard('')
+
+class NotesReverseQuoteCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+
+        self.view.run_command('select_all')
+        content = self.view.substr(self.view.sel()[0]).strip()
+
+
+        pa = r'((##(\s)*)[^#]*(###(\s)*)[^#]*)'
+        pa = re.compile(pa, re.M)
+        res = pa.findall(content)
+
+        content = ''
+        print(len(res))
+        for i in range(len(res) - 1, -1, -1):
+            content = content + res[i][0] + '\n\n'
+            print(i)
+
+
+       
+
+        self.view.run_command('cut')
+        self.view.insert(edit, 0, content)
+        sublime.set_clipboard('')
+
